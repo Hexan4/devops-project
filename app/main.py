eb98904f41a11b7369d80f9088121fe35842940f
Krystian <126709189+Hexan4@users.noreply.github.com>
2025-01-03 19:24:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
- 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>test flask siema</h1>
+    <p>test stronki oby bylo git</p>
+    """
+
+@app.route("/hello")
+def hello():
+    return "Hello from Flask!"
+
+if __name__ == "__main__":
+    # Uruchomienie aplikacji na porcie 5000
+    app.run(host="0.0.0.0", port=5000)
